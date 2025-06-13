51662da83a3f2d6362022cb5a30c2f33182cf512
ASM-Monirul-Islam <monirul.01401411046@gmail.com>
2025-06-13 17:11:43 +0600

    Learning JS

---
+++ 01_Variable_DataTypes_Objects.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 6;
+let c = "Sayem"
+console.log(a+b);
+
+console.log(typeof a, typeof b, typeof c)
+
+const a1 = 10;
+// a1 = a1+1; //Not Allowed
+
+var x = 5;
+
+{
+	var x = 15;
+	console.log(x)
+}
+
+console.log(x)
