3496a160ca8cf89ae142d12f9004316d3091b946
ASM-Monirul-Islam <monirul.01401411046@gmail.com>
2025-06-13 17:15:01 +0600

    Changed some text

--- 01_Variable_DataTypes_Objects.js
+++ 01_Variable_DataTypes_Objects.js
@@ -1,18 +1,25 @@
 let a = 5;
 let b = 6;
 let c = "Sayem"
-console.log(a+b);
+console.log(a+b); //11
 
-console.log(typeof a, typeof b, typeof c)
+console.log(typeof a, typeof b, typeof c) //number number string
 
-const a1 = 10;
+//------------------------------------------------
+const a1 = 10; //The value is Constant
 // a1 = a1+1; //Not Allowed
+//------------------------------------------------
 
+// -----------------------------------------------
+// var is global scoped
+// while the let is blocked scoped
 var x = 5;
-
 {
-	var x = 15;
-	console.log(x)
+	var x = 15; //It changes the value of x to 15
+	console.log(x) //15
 }
+console.log(x) //15
+// -----------------------------------------------
 
-console.log(x)
+
+// -----------------------------------------------
